80be906dde3edc80f65c4c4b48a00c14c29895d9
brianrego03 <brian.rego03@gmail.com>
2025-03-11 17:23:06 +0530

    Create async function inside a useeffect to fetch pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const pokemonArray=["pikachu","squirtle","charmander","onix","bulbasaur","caterpie",
+    "rattata","spearow","nidorino","clefairy","jigglypuff","mewtwo"];
+  const pokeUrl="https://pokeapi.co/api/v2/pokemon/";  
+  const [pokemonInfoArray,setPokemonInfoArray]=useState([]);
+  useEffect(()=>{
+    async function getPokemon(){
+      const promises=pokemonArray.map(pokemon=>fetch(pokeUrl+pokemon).then(res=>res.json()));
+      const pokemonData= await Promise.all(promises);
+      setPokemonInfoArray(pokemonData);
+      console.log(pokemonData[0].name)
+
+    }
+    getPokemon();
+
+  }, []);
+
+  // console.log(pokemonInfoArray);
 
   return (
     <>
